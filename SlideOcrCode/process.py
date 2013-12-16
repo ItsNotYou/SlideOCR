73353d4e6677943c70d85dae6058b79b19e36203
Hendrik Geßner <HENGE01@gmail.com>
2013-12-16 16:42:56 +0100

    Inserted bilateral filtering pre processing into process chain.

--- SlideOcrCode/process.py
+++ SlideOcrCode/process.py
@@ -8,14 +8,18 @@ import argparse
 import os
 from slideocr.ocr.Abbyy import AbbyyCloud
 from slideocr.Data import OcrImage
+from slideocr.PreProcessors import BilateralFiltering
 
 
 def recognizeFile(sourceFile):
     images = [OcrImage()]
     images[0].path = sourceFile
     
+    biFilter = BilateralFiltering(75)
+    images = biFilter.process(images)
+    
     abbyy = AbbyyCloud()
-    abbyy.process(images)
+    images = abbyy.process(images)
     
     print images[0].text
 
