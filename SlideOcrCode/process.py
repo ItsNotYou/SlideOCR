b5abc1a2d9fb83478a09b43886d77ff69319020b
Hendrik Geßner <HENGE01@gmail.com>
2013-12-16 18:49:20 +0100

    Added sample image / working directory separation. Inserted tesseract ocr into process.

--- SlideOcrCode/process.py
+++ SlideOcrCode/process.py
@@ -6,7 +6,10 @@ Created on 08.12.2013
 
 import argparse
 import os
+import copy
+import shutil
 from slideocr.ocr.Abbyy import AbbyyCloud
+from slideocr.ocr.Tesseract import Tesseract
 from slideocr.Data import OcrImage
 from slideocr.PreProcessors import BilateralFiltering
 
@@ -18,21 +21,32 @@ def recognizeFile(sourceFile):
     biFilter = BilateralFiltering(75)
     images = biFilter.process(images)
     
-    abbyy = AbbyyCloud()
-    images = abbyy.process(images)
+    imagesAbbyy = copy.deepcopy(images);
+    imagesTesseract = copy.deepcopy(images);
     
-    print images[0].text
+    abbyy = AbbyyCloud()
+    result = abbyy.process(imagesAbbyy)
+     
+    print result[0].text
+    
+    tesseract = Tesseract()
+    result = tesseract.process(imagesTesseract)
+    
+    print result[0].text
 
 
 parser = argparse.ArgumentParser( description="Recognize a file via web service" )
 parser.add_argument( 'sourceFile' )
+parser.add_argument( 'workingDirectory' )
 args = parser.parse_args()
 
 sourceFile = args.sourceFile
+workingDirectory = args.workingDirectory
 
 if os.path.isfile(sourceFile):
-    print("Parse Datei")
-    recognizeFile(sourceFile)
+    targetFile = os.path.join(workingDirectory, os.path.basename(sourceFile));
+    shutil.copyfile(sourceFile, targetFile)
+    recognizeFile(targetFile)
 else:
     print(sourceFile + " ist keine Datei")
     print("Vorgang abgebrochen")
