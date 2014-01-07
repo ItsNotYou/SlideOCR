61191f3f35aec847a5064d743d21e3f22dbacbb4
Matthias <Matthias@141.89.154.54>
2014-01-07 16:45:27 +0100

    Merging of bounding boxes works

--- SlideOcrCode/slideocr/BoxMerging.py
+++ SlideOcrCode/slideocr/BoxMerging.py
@@ -14,46 +14,63 @@ class Merging():
     def process(self,images,threshold):
     
         currentBoundingArray = []
-    
+        
         for image in images:
+            currentBoundingArray.append(image.bounding)
             
-            newBoundingArray = []
-            
-            flag = True
-            
-            for currentBounding in currentBoundingArray:
-                
-                if (_nextTo(currentBounding,image.bounding,threshold) and _ifSameLine(currentBounding,image.bounding)):
-                    
-                    newBoundingBox = BoundingBox()
-                    newBoundingBox.left = min(currentBounding.left,image.bounding.left)
-                    newBoundingBox.right = max(currentBounding.right,image.bounding.right)
-                    newBoundingBox.top = min(currentBounding.top,image.bounding.top)
-                    newBoundingBox.bottom = max(currentBounding.bottom,image.bounding.bottom)
-                    
-                    newBoundingArray.append(newBoundingBox)
-                    flag = False
-                    
-                else:
-                    newBoundingArray.append(currentBounding)
-            
-            if (flag):
-                newBoundingArray.append(copy.deepcopy(image.bounding))
-                
-                
-            currentBoundingArray = newBoundingArray
-            flag = True
+        newBounding = _merge(currentBoundingArray,threshold)
+        
+        newSize = 0
+        keks = _merge(newBounding,threshold)
+        oldSize = len(keks)
+        while (newSize != oldSize):
+            oldSize = newSize
+            keks = _merge(keks,threshold)
+            newSize = len(keks)
             
         inImage = cv2.imread("C:\\Users\\Matthias\\Documents\\Workspace\\Python\\SlideOCR\\samples\\working\\first_slide.png", 1)
+    
+        for b in keks:
         
-        for b in currentBoundingArray:
-            
             cv2.rectangle(inImage, (b.left, b.top), (b.right, b.bottom), (0, 0, 255), 2) 
-            
+        
         cv2.imwrite("C:\\Users\\Matthias\\Documents\\Workspace\\Python\\SlideOCR\\samples\\working\\bllaaaaass.png",inImage)
         
         
+def _merge(oldBoundings,threshold):
+    
+    currentBoundingArray = []
+
+    for oldBounding in oldBoundings:
+        
+        newBoundingArray = []
+        
+        flag = True
+        
+        for currentBounding in currentBoundingArray:
             
+            if ((_nextTo(currentBounding,oldBounding,threshold) or _inIt(currentBounding,oldBounding)) and _ifSameLine(currentBounding,oldBounding)):
+                
+                newBoundingBox = BoundingBox()
+                newBoundingBox.left = min(currentBounding.left,oldBounding.left)
+                newBoundingBox.right = max(currentBounding.right,oldBounding.right)
+                newBoundingBox.top = min(currentBounding.top,oldBounding.top)
+                newBoundingBox.bottom = max(currentBounding.bottom,oldBounding.bottom)
+                
+                newBoundingArray.append(newBoundingBox)
+                flag = False
+                
+            else:
+                newBoundingArray.append(currentBounding)
+        
+        if (flag):
+            newBoundingArray.append(copy.deepcopy(oldBounding))
+            
+            
+        currentBoundingArray = newBoundingArray;
+        flag = True     
+        
+    return currentBoundingArray         
                 
  
 def _nextTo(bounding1,bounding2,threshold):
@@ -61,7 +78,7 @@ def _nextTo(bounding1,bounding2,threshold):
     if (math.fabs(bounding1.right - bounding2.left) < threshold or \
         math.fabs(bounding2.right - bounding1.left) < threshold ):
         
-        return True;
+        return True
     
     return False               
 
@@ -71,12 +88,49 @@ def _ifSameLine(bounding1,bounding2):
     if (bounding1.top >= bounding2.top and bounding1.top <= bounding2.bottom or \
         bounding2.top >= bounding1.top and bounding2.top <= bounding1.bottom):
         
-        return True;
+        return True
     
-    return False;
+    return False
+
+def _inIt(bounding1,bounding2):
+    if (bounding1.left >= bounding2.left and bounding1.left <= bounding2.right or \
+        bounding1.right >= bounding2.left and bounding1.right <= bounding2.right or \
+        bounding2.left >= bounding1.left and bounding2.left <= bounding1.right or \
+        bounding2.right >= bounding1.left and bounding2.right <= bounding1.right):
         
-        
+        return True
     
+    return False
+
+def _checkForNewMerges(boundingBox,boundingBoxArray,threshold):
+    
+    flag = True
+    
+    newBoundingArray = []
+    newBoundingBox = BoundingBox()
+    
+    for currentBoundingBox in boundingBoxArray:
+        
+        if ((_nextTo(currentBoundingBox,boundingBox,threshold) or _inIt(currentBoundingBox,boundingBox)) and _ifSameLine(currentBoundingBox,boundingBox) and flag):
+        
+            newBoundingBox.left = min(currentBoundingBox.left,boundingBox.left)
+            newBoundingBox.right = max(currentBoundingBox.right,boundingBox.right)
+            newBoundingBox.top = min(currentBoundingBox.top,boundingBox.top)
+            newBoundingBox.bottom = max(currentBoundingBox.bottom,boundingBox.bottom)
+            
+            newBoundingArray.append(newBoundingBox)
+            flag = False
+    
+        else:
+            newBoundingArray.append(currentBoundingBox)
+            
+    if (flag):
+        newBoundingArray.append(copy.deepcopy(boundingBox))
+        return newBoundingArray
+    
+    else:
+        return _checkForNewMerges(newBoundingBox,newBoundingArray,threshold)
+        
     
     
                 
